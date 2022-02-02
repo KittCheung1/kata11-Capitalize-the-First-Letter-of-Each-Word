9b458ae8eb4b020112a2f8df1b17dbc2b9e2c565
Kitt Cheung <kitt.cheung@se.experis.com>
2022-02-02 10:28:12 +0100

    kata11-edabit

---
+++ app.js
@@ -0,0 +1,19 @@
+function makeTitle(string) {
+    let newStr = []
+    let str = string.split(" ")
+
+for (let i = 0; i < str.length; i++) {
+console.log(str[i][0].toUpperCase())
+}
+    // console.log(str)
+    // console.log(str)
+
+};
+
+
+
+
+
+
+
+makeTitle("This is a title");
